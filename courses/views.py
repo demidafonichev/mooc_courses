bf007e0d1453899e18e1ac055f613ea34f73ea03
Demid Afonichev <demidafonichev@gmail.com>
2018-04-14 18:21:00 +0300

    Refactor course_catalog view and get_course view to send image urls

--- courses/views.py
+++ courses/views.py
@@ -125,7 +125,7 @@ def get_course(request, course_id):
             })
         slides.append({"id": slide.pk,
                        "number": slide.number,
-                       "image": slide.image,
+                       "image": slide.image.url,
                        "comments": comments})
 
     check_points = []

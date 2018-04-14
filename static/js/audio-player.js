99525f790515d6e0383bc80a7fe948181724dd55
Demid Afonichev <demidafonichev@gmail.com>
2018-04-15 00:26:34 +0300

    Make first check point time cell a div (cant change as others) + refactor

--- static/js/audio-player.js
+++ static/js/audio-player.js
@@ -146,7 +146,6 @@ function changeVolume(event) {
 }
 
 function formatTimeToString(time) {
-    console.log("here");
     let min = Math.floor(time / 60);
     let sec = Math.floor(time % 60);
     return min + ':' + (sec < 10 ? '0' + sec : sec);

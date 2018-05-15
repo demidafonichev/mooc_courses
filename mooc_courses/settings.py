583449e0699e29c933038fe9ae722d368b8f8a5e
Demid Afonichev <demidafonichev@gmail.com>
2018-05-16 01:29:48 +0300

    Starting recoding to React

--- mooc_courses/settings.py
+++ mooc_courses/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webpack_loader',
     'courses',
-    'accounts'
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -86,6 +87,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': os.path.join(BASE_DIR, 'static/bundles/local/'),
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats-local.json'),
     }
 }
 
---
+++ webpack.base.config.js
@@ -0,0 +1,32 @@
+var path = require("path")
+var webpack = require('webpack')
+
+module.exports = {
+  context: __dirname,
+
+  entry: {
+    Main: './react/Main',
+    vendors: ['react'],
+  },
+
+  output: {
+      path: path.resolve('./static/bundles/local/'),
+      filename: "[name]-[hash].js"
+  },
+
+  externals: [
+  ], // add all vendor libs
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+  ], // add all common plugins here
+
+  module: {
+    loaders: [] // add all common loaders here
+  },
+
+  resolve: {
+    modulesDirectories: ['node_modules', 'bower_components'],
+    extensions: ['', '.js', '.jsx']
+  },
+}
